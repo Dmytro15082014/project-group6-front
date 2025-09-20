0e28fc02ba47903ec5194604ad3d427ab528c6b7
sofia-kindratyshyn <sofia25kind@gmail.com>
2025-09-20 17:22:00 +0300

    added some changes

--- app/(private routes)/journey/[weekNumber]/page.tsx
+++ app/(private routes)/journey/[weekNumber]/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
+import ToggleTabs from "@/components/ui/Tabs";
+import WeekSelector from "@/components/WeekSelector/WeekSelector";
+import React, { useMemo } from "react";
 
 const page = () => {
-  return <div>page</div>;
+  const pregnacyWeeks = useMemo(() => {
+    let weekNumbers: number[] = [];
+    for (let i = 0; i <= 11; i++) {
+      weekNumbers.push(i);
+    }
+    return weekNumbers;
+  }, []);
+  return (
+    <div>
+      <h1>Доброго ранку, Галина!</h1>
+      <WeekSelector weeks={pregnacyWeeks} />
+      <ToggleTabs options={["Розвиток малюка", "Тіло мами"]} />
+    </div>
+  );
 };
 
 export default page;
